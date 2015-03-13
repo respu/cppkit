848d4f18b1ec2eaa15bd15b774700b741aed0604
Tony Di Croce <td@cheapo.home.network>
2015-03-13 16:51:11 -0700

    added file lock. need to try on other platforms

---
+++ source/ck_file_lock.cpp
@@ -0,0 +1,71 @@
+
+#include "cppkit/ck_file_lock.h"
+#include "cppkit/os/ck_error_msg.h"
+
+#ifdef IS_WINDOWS
+#include <windows.h>
+#else
+#include <sys/file.h>
+#endif
+
+using namespace cppkit;
+
+ck_file_lock::ck_file_lock( int fd ) :
+    _fd( fd )
+{
+}
+
+ck_file_lock::~ck_file_lock() noexcept
+{
+}
+
+void ck_file_lock::lock( bool exclusive )
+{
+#ifdef IS_WINDOWS
+    intptr_t fileHandle = _get_osfhandle( _fd );
+    if( fileHandle == INVALID_HANDLE_VALUE )
+        CK_THROW(("Unable to get OS handle."));
+
+    OVERLAPPED overlapped;
+    overlapped.Offset = 0;
+    overlapped.OffsetHigh = 0;
+
+    BOOL success = LockFileEx( fileHandle,
+                               (exclusive) ? LOCKFILE_EXCLUSIVE_LOCK : 0,
+                               0,
+                               MAXDWORD,
+                               MAXDWORD,
+                               &overlapped );
+    if( !success )
+        CK_THROW(("Unable to acquire file lock: %s",ck_get_last_error_msg().c_str()));
+#else
+    int err = flock( _fd, (exclusive)?LOCK_EX:LOCK_SH );
+    if( err < 0 )
+        CK_THROW(("Unable to acquire file lock: %s",ck_get_last_error_msg().c_str()));
+#endif
+}
+
+void ck_file_lock::unlock()
+{
+#ifdef IS_WINDOWS
+    intptr_t fileHandle = _get_osfhandle( _fd );
+    if( fileHandle == INVALID_HANDLE_VALUE )
+        CK_THROW(("Unable to get OS handle."));
+
+    OVERLAPPED overlapped;
+    overlapped.Offset = 0;
+    overlapped.OffsetHigh = 0;
+
+    BOOL success = UnlockFileEx( fileHandle,
+                                 0,
+                                 MAXDWORD,
+                                 MAXDWORD,
+                                 &overlapped );
+    if( !success )
+        CK_THROW(("Unable to release file lock: %s",ck_get_last_error_msg().c_str()));
+#else
+    int err = flock( _fd, LOCK_UN );
+    if( err < 0 )
+        CK_THROW(("Unable to release file lock: %s",ck_get_last_error_msg().c_str()));
+#endif
+}
